0d692d37a53859e27e626f1b8d332d5d01eed2d5
Israel Meir <i0509711452@gmail.com>
2026-06-25 14:33:36 +0300

    1-3

---
+++ while loop .py
@@ -0,0 +1,19 @@
+# 1
+i=0
+while i <=5:
+    print(i)
+    i+=1
+# 2
+count=10
+while count >= 1:
+    print(count)
+    count-=1
+# 3
+count = 1
+total = 0
+while count <= 10:
+    total = total + count
+    count += 1
+
+print(total)
+
